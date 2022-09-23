e7ad3993100b5c0d55021e2a2e60a0d6c4561f5b
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-09-23 18:23:20 +0000

    Create and finish styling navbar for the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import React from 'react';
+import Home from './pages/Home';
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <>
+      <Home />
+    </>
+  )
 };
 
 export default App;
